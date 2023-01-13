f1888ee85a63a65a2ee6f6a882fbf6170dbe1cde
NID_desktop <ademiltonsousacarvalho@protonmail.com>
2023-01-12 21:40:12 -0300

    Trabalhando no design
    Gastei quase 1 hora redescobrindo como se faz tudo nesse tkinter. Mas que raios de biblioteca, toda santa vez eu esqueço a maior parte doque aprendi e tenho que ficar pesquisando. Pratica faz a perfeição, vamos botar isso a prova

--- main.py
+++ main.py
@@ -45,20 +45,52 @@ def cli():
 
 class app:
     def __init__(self, master=None):
-        root = Tk()
-        root.title("Vigenere Cipher")
-        root.geometry("500x750+30+30")
-        row1 = Frame(root)
-        row2 = Frame(root)
-        row3 = Frame(root)
-        row4 = Frame(root)
+        self.root = Tk()
+        self.root.title("Vigenere Cipher")
+        self.root.geometry("500x450+30+30")
+        self.root.maxsize(500, 450)
+        self.root.resizable(0, 0)
+        self.root.config(bg="black")
 
-        row1.pack()
-        row2.pack()
-        row3.pack()
-        row4.pack()
+        self.row1 = Frame(self.root, height=100)
+        self.row1.pack_propagate(0)
+        self.row2 = Frame(self.root, height=100)
+        self.row2.pack_propagate(0)
+        self.row3 = Frame(self.root, height=100)
+        self.row3.pack_propagate(0)
+        self.row3.columnconfigure(0, weight=1)
+        self.row3.columnconfigure(1, weight=1)
+        self.row4 = Frame(self.root, height=100)
+        self.row4.pack_propagate(0)
 
-        root.mainloop()
+        self.row1.pack(pady=(50,0), fill="both")
+        self.row2.pack(fill="both")
+        self.row3.pack(fill="both")
+        self.row4.pack(fill="both")
+
+        # Mensagem
+        self.msgV = StringVar(self.root, value="Sua mensagem")
+        self.msg = Entry(self.row1, font=("verdana", 20))
+        self.msg.pack()
+
+        # Key
+        self.keyV = StringVar(self.root, value="key")
+        self.key = Entry(self.row2, font=("verdana", 20))
+        self.key.pack()
+        
+        # Encriptar ou Decriptar
+        self.btn = Button(self.row3)
+        self.btn.grid(row=0, column=1, sticky="WE")
+
+        self.btn2 = Button(self.row3)
+        self.btn2.grid(row=0, column=2, sticky="WE")
+        
+        # Mensagem 2
+        self.msg2V = StringVar(self.root, value="Criptado")
+        self.msg2 = Entry(self.row4, font=("verdana", 20))
+        self.msg2.pack()        
+
+        self.root.mainloop()
     
 
 if __name__ == "__main__":
