e2eb0c9db5c9f132094d97e705ca67cb43b27fd6
NID_desktop <ademiltonsousacarvalho@protonmail.com>
2023-01-15 21:41:52 -0300

    Design e funcionamento
    O design foi melhorado, com adição de cores e formato dos botões. Alem da fonte, é claro. Não gosto muito de mexer com a estetica, mas dessa vez eu vou tentar entregar esticamento viavel.\n O botão de encriptar ta funcionando perfeitamente.

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from tkinter import *
 # vigenere  cipher
 
-def cli():
+def cipherSolver(msg, key, tipo):
     alpha_map = [
         'a', 'b', 'c', 'd', 'e', 'f', 'g',
         'h', 'i', 'j', 'k', 'l', 'm', 'n',
         'o', 'p', 'q', 'r', 's', 't', 'u',
-        'v', 'w', 'x', 'y', 'z'
+        'v', 'w', 'x', 'y', 'z', ' '
         ]
 
-    msg = input("Digite a cifra ou a mensagem a ser cifrada:").lower()
-    key = input("Digite a Chave de Encriptação: ").lower()    
-    resp = int(input("encriptar(1) ou desencriptar(0): "))
-        
+    msg = ''.join(list(char for char in msg.lower() if char in alpha_map))
+    key = ''.join(key.split(' ')).lower()
+    resp = tipo
+
     msg2 = ''
     key_aux = 0
     for char in msg:
@@ -26,12 +26,14 @@ def cli():
         key_i = alpha_map.index(key[key_aux])
         
         # encripta ou decripta o char
-        if resp:
+        if not resp:
             soma = msg_i + key_i
         else:
             soma = msg_i - key_i
-        if soma> 26:
+
+        if soma>= 26:
             soma -= 26
+        
         msg2 += alpha_map[soma]
 
         # incremento do indice da chave
@@ -40,27 +42,37 @@ def cli():
         else:
             key_aux += 1
     print(msg2)
+    return msg2
 
 
 
 class app:
     def __init__(self, master=None):
+        # Constantes
+        self.BG_COLOUR = "#bbaabb"
+        self.BUTTON_BG_COLOUR = "#aa1a44"
+        self.BUTTON_BG_COLOUR = "#aaaaff"
+        self.FONT_COLOR = "#111177"
+
+        # configuração do root
         self.root = Tk()
         self.root.title("Vigenere Cipher")
         self.root.geometry("500x450+30+30")
         self.root.maxsize(500, 450)
         self.root.resizable(0, 0)
-        self.root.config(bg="black")
+        self.root.config(bg=self.BG_COLOUR)
 
-        self.row1 = Frame(self.root, height=100)
+
+        # Confifurando as Rows
+        self.row1 = Frame(self.root, height=100, bg=self.BG_COLOUR)
         self.row1.pack_propagate(0)
-        self.row2 = Frame(self.root, height=100)
+        self.row2 = Frame(self.root, height=100, bg=self.BG_COLOUR)
         self.row2.pack_propagate(0)
-        self.row3 = Frame(self.root, height=100)
+        self.row3 = Frame(self.root, height=100, bg=self.BG_COLOUR)
         self.row3.pack_propagate(0)
         self.row3.columnconfigure(0, weight=1)
         self.row3.columnconfigure(1, weight=1)
-        self.row4 = Frame(self.root, height=100)
+        self.row4 = Frame(self.root, height=100, bg=self.BG_COLOUR)
         self.row4.pack_propagate(0)
 
         # Container position
@@ -71,21 +83,26 @@ class app:
 
         # Mensagem
         self.msgV = StringVar(self.root, "Sua mensagem: ")
-        self.msg = Entry(self.row1, font=("verdana", 20), textvariable=self.msgV)
+        self.msg = Entry(self.row1, font=("verdana", 20), textvariable=self.msgV, fg=self.FONT_COLOR)
         self.msg.bind("<FocusIn>", self.handle_focus)
 
         # Key
         self.keyV = StringVar(self.root, value="Sua key")
-        self.key = Entry(self.row2, font=("verdana", 20), textvariable=self.keyV)
+        self.key = Entry(self.row2, font=("verdana", 20), textvariable=self.keyV, fg=self.FONT_COLOR)
         self.key.bind("<FocusIn>", self.handle_focus)
         
         # Encriptar ou Decriptar Buttons
-        self.btn = Button(self.row3, text="oi")
-        self.btn2 = Button(self.row3)
-        
+        self.btn = Button(self.row3, text="ENCRIPTAR", font=("verdana 12 bold"), height=2, fg=self.FONT_COLOR,
+                            bg=self.BUTTON_BG_COLOUR, command = lambda: self.criptacao(0)
+                            )
+        self.btn2 = Button(self.row3, text="DECRIPTAR", font=("verdana 12 bold"), height=2, fg=self.FONT_COLOR,
+                            bg=self.BUTTON_BG_COLOUR, command = lambda: self.criptacao(1)
+                            )
+                                    
         # Mensagem 2
         self.msg2V = StringVar(self.root, value="Criptado")
-        self.msg2 = Entry(self.row4, font=("verdana", 20))
+        self.msg2 = Entry(self.row4, font=("verdana", 20), fg=self.FONT_COLOR, state="readonly",
+                        textvariable=self.msg2V)
 
         # Position/ Packing
         self.msg.pack(expand="true")
@@ -101,6 +118,15 @@ class app:
             self.msgV.set("")
         if event.widget == self.key:
             self.keyV.set("")
+
+    def criptacao(self, tipo=0):
+        # Função dos botões
+        # tipo 0 é pra encripitar e 1 pra decriptar
+        valor1 = self.msgV.get()
+        valor2 = self.keyV.get()
+        valor3 = cipherSolver(valor1, valor2, tipo)
+        self.msg2V.set(valor3)
+
 
 if __name__ == "__main__":
     app()
